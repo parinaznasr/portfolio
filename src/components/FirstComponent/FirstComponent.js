0ebefb686a0f8fafb080e0eea67b8edbf869f578
Mahan <mahanmahdipour1379@gmail.com>
2022-12-23 18:58:04 -0500

    Initial commit

---
+++ src/components/FirstComponent/FirstComponent.js
@@ -0,0 +1,21 @@
+import React, {
+  Component
+} from "react";
+import "./FirstComponent.css";
+
+const FirstComponent = (props) => {
+
+    return ( 
+      <div className="body">
+        <div className="title">
+          PORTFOLIO
+          <div className="name">
+            Yasaman Pazoki Toroudi
+          </div>
+        </div>
+        
+      </div>
+    );
+    }
+
+    export default FirstComponent;
