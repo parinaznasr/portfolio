a3f599777d9b264e0ecc2f351785ad9f00d09500
parinaznasr <parinaz.nasresfahani@gmail.com>
2023-05-12 23:36:48 -0400

    Updated

--- src/Pages/CV/personalskills/personalskills.js
+++ src/Pages/CV/personalskills/personalskills.js
@@ -28,24 +28,24 @@ const Personalskills = () => {
           {/*    </Grid>*/}
           {/*</Container>*/}
           {/*<Container>*/}
-          <Grid container spacing={2}>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
               <Grid xs={12}>
                   <h2 className="skills-title">Soft Skills</h2>
               </Grid>
           </Grid>
-          <Grid container spacing={1}>
-              <Grid xs={2}/>
-              <Grid xs={4}>
+          <Grid container spacing={{xs: 2, md: 2}}>
+              <Grid xs={1} sm={2} md={2}/>
+              <Grid xs={11} sm={4} md={4} >
                   <ul className="skills-list">
                       { skills.slice(0,(skills.length)/2).map((item, index)=> {return(<li key={index}>{item}</li>)})}
                   </ul>
               </Grid>
-              <Grid xs={5}>
+              <Grid xs={12} sm={5} md={4}>
                   <ul className="skills-list">
                       { skills.slice((skills.length)/2, skills.length).map((item, index)=> {return(<li key={index}>{item}</li>)})}
                   </ul>
               </Grid>
-              <Grid xs={1}/>
+              <Grid xs={1} sm={1} md={2}/>
           </Grid>
           {/*</Container>*/}
       </div>
