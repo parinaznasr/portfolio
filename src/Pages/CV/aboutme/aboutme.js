93d36a1befad24c341df25f293cfa26d99ed73e3
parinaznasr <parinaz.nasresfahani@gmail.com>
2023-05-12 23:43:34 -0400

    Fixed about me

--- src/Pages/CV/aboutme/aboutme.js
+++ src/Pages/CV/aboutme/aboutme.js
@@ -7,17 +7,21 @@ const aboutme = () => {
   return (
       <StyledSection>
           <div className="aboutme-container">
-              <Grid container spacing={12}>
-                  <Grid xs={2}/>
-                  <Grid xs={8}>
-                      <div className="aboutme-title">
-                          About Me
-                      </div>
+              <Grid container spacing={2}>
+                  <Grid item xs={1} md={2} />
+                  <Grid item xs={10} md={8}>
+                      <div className="aboutme-title">About Me</div>
                       <div className="aboutme-description">
-                          As an architecture student, my objective is to secure a summer internship that aligns with my field of interest, such as sustainable design, while utilizing my relevant skills in Rhino, Grasshopper, AutoCad, and Adobe Creative Suite. I am eager to gain valuable experience in specific aspects of the field, such as green building practices and urban planning, while contributing to a dynamic team and expanding my professional network.
+                          As an architecture student, my objective is to secure a summer
+                          internship that aligns with my field of interest, such as
+                          sustainable design, while utilizing my relevant skills in Rhino,
+                          Grasshopper, AutoCad, and Adobe Creative Suite. I am eager to gain
+                          valuable experience in specific aspects of the field, such as
+                          green building practices and urban planning, while contributing to
+                          a dynamic team and expanding my professional network.
                       </div>
                   </Grid>
-                  <Grid xs={2}/>
+                  <Grid item xs={1} md={2} />
               </Grid>
           </div>
       </StyledSection>
